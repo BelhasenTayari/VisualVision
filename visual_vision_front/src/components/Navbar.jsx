3b6b7d9d00bec211b48e42502949e5f34278a11b
HaithemBenSlama <haithem.benslama@hotmail.fr>
2024-04-09 13:24:12 +0100

    Create Home page and Navbar

---
+++ visual_vision_front/src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+"use client";
+import React, { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+import { logo } from "../../public/assets";
+
+const Navbar = () => {
+  const menuItems = [
+    { name: "Home", href: "/" },
+    { name: "Documentation", href: "/doc" },
+    { name: "Visual Vision WEB", href: "/classifier" },
+    { name: "About Us", href: "/about" },
+  ];
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-deep-700 border-gray-200 shadow-xl">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img
+            src="https://i.postimg.cc/FK6QK1yW/logo-3.png"
+            className="h-8 w-8"
+            alt="Logo"
+          />
+          <span className="self-center text-2xl  whitespace-nowrap text-gray-200 font-oswald font-semibold">
+            Visual vision
+          </span>
+        </a>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-300 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+
+          <IoMenu className="w-8 h-8" />
+        </button>
+        <div
+          className={`${isOpen ? "" : "hidden"} w-full md:block md:w-auto`}
+          id="navbar-default"
+        >
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent">
+            {menuItems.map((item) => (
+              <li key={item.name}>
+                <a
+                  href={item.href}
+                  className="block py-2 px-3 text-gray-900 md:text-gray-200 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- visual_vision_front/tailwind.config.js
+++ visual_vision_front/tailwind.config.js
@@ -7,10 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        oswald: ["Oswald", "sans-serif"],
+      },
+      colors: {
+        deep: {
+          300: "#3CA6A6",
+          500: "#026773",
+          700: "#024959",
+          900: "#012E40",
+        },
+        teal: "#F2E3D5",
       },
     },
   },
