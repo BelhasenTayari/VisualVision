14918a84ce525aca36ab8a4976bd5be5d1f2afb8
ahmedhamila <csahmed17@gmail.com>
2024-04-15 18:35:25 +0100

    adding CM & Architecture to AIModels

--- visual_vision_back/api/models.py
+++ visual_vision_back/api/models.py
@@ -11,6 +11,8 @@ class AIModel(models.Model):
     accuracy = models.DecimalField(decimal_places=2,max_digits=5,default=0.0)
     macro_avg = models.DecimalField(decimal_places=2,max_digits=5,default=0.0)
     wieghted_avg = models.DecimalField(decimal_places=2,max_digits=5,default=0.0)
+    confusion_matrix = models.ImageField(upload_to='images/',null=True)
+    architecture = models.JSONField(default=dict)
     model_file = models.FileField(upload_to='models/')
     created_at = models.DateTimeField(auto_now_add=True)
 
