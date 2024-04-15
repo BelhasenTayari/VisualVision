7eb2e61f2526e1246bc566b0b7f22905859a7f0a
miledyessine <miled.yassine7@gmail.com>
2024-04-15 17:46:43 +0100

    about page + doc

--- visual_vision_front/src/app/doc/page.jsx
+++ visual_vision_front/src/app/doc/page.jsx
@@ -1,7 +1,193 @@
-import React from "react";
+"use client";
+import Navbar from "@/components/Navbar";
+import React, { useState } from "react";
+import data from "./models.json";
 
-const page = () => {
-  return <div>page</div>;
+const Page = () => {
+    const [selectedAlgorithm, setSelectedAlgorithm] = useState("");
+    const [selectedModel, setSelectedModel] = useState("");
+
+    const handleAlgorithmChange = (event) => {
+        setSelectedAlgorithm(event.target.value);
+    };
+
+    const handleModelChange = (event) => {
+        setSelectedModel(event.target.value);
+    };
+
+    return (
+        <div className="min-h-screen bg-deep-900 ">
+            <Navbar />
+            <h3 className="m-10 mb-2 text-2xl font-bold text-white">
+                Documentation :
+            </h3>
+
+            <div className="inline-flex ml-8 items-center justify-center py-2.5 px-4 text-sm font-medium text-gray-700">
+                <label htmlFor="algos" className="sr-only">
+                    Choose an algorithm
+                </label>
+                <select
+                    id="algos"
+                    value={selectedAlgorithm}
+                    onChange={handleAlgorithmChange}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg border-gray-100 dark:border-gray-700 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:ring-blue-500 focus:border-blue-500 block w-max p-2.5 mr-3"
+                >
+                    <option value="" disabled>
+                        Choose an algorithm
+                    </option>
+                    {data.algorithms.map((algorithm, index) => (
+                        <option key={index} value={algorithm.name}>
+                            {algorithm.name}
+                        </option>
+                    ))}
+                </select>
+
+                <label htmlFor="models" className="sr-only">
+                    Choose a model
+                </label>
+                <select
+                    id="models"
+                    value={selectedModel}
+                    onChange={handleModelChange}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg border-gray-100 dark:border-gray-700 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:ring-blue-500 focus:border-blue-500 block w-max p-2.5"
+                >
+                    <option value="" disabled>
+                        Choose a model
+                    </option>
+                    {selectedAlgorithm &&
+                        data.algorithms
+                            .find((algo) => algo.name === selectedAlgorithm)
+                            .models.map((model, index) => (
+                                <option key={index} value={model.name}>
+                                    {model.name}
+                                </option>
+                            ))}
+                </select>
+            </div>
+
+            <section className=" p-3 sm:p-5">
+                <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
+                    {selectedModel && (
+                        <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+                            <div className="flex flex-col text-white font-bold md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+                                Model: {selectedModel}
+                            </div>
+                            <div className="overflow-x-auto">
+                                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th
+                                                scope="col"
+                                                className="px-4 py-3"
+                                            >
+                                                Layer (Type)
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="px-4 py-3"
+                                            >
+                                                Output Shape
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="px-4 py-3"
+                                            >
+                                                Param #
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {data.algorithms
+                                            .find(
+                                                (algo) =>
+                                                    algo.name ===
+                                                    selectedAlgorithm
+                                            )
+                                            .models.find(
+                                                (model) =>
+                                                    model.name === selectedModel
+                                            )
+                                            .table.map((item, index) => (
+                                                <tr
+                                                    key={index}
+                                                    className="border-b dark:border-gray-700"
+                                                >
+                                                    <td className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        {item.layer}
+                                                    </td>
+                                                    <td className="px-4 py-3">
+                                                        {item.shape}
+                                                    </td>
+                                                    <td className="px-4 py-3">
+                                                        {item.param}
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                            <nav className="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4">
+                                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                                    Total params:
+                                    <span className="font-semibold text-gray-900 dark:text-white">
+                                        {
+                                            data.algorithms
+                                                .find(
+                                                    (algo) =>
+                                                        algo.name ===
+                                                        selectedAlgorithm
+                                                )
+                                                .models.find(
+                                                    (model) =>
+                                                        model.name ===
+                                                        selectedModel
+                                                ).summary.params
+                                        }
+                                    </span>
+                                </span>
+                                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                                    Trainable params:
+                                    <span className="font-semibold text-gray-900 dark:text-white">
+                                        {
+                                            data.algorithms
+                                                .find(
+                                                    (algo) =>
+                                                        algo.name ===
+                                                        selectedAlgorithm
+                                                )
+                                                .models.find(
+                                                    (model) =>
+                                                        model.name ===
+                                                        selectedModel
+                                                ).summary.trainable
+                                        }
+                                    </span>
+                                </span>
+                                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                                    Non-trainable params:
+                                    <span className="font-semibold text-gray-900 dark:text-white">
+                                        {
+                                            data.algorithms
+                                                .find(
+                                                    (algo) =>
+                                                        algo.name ===
+                                                        selectedAlgorithm
+                                                )
+                                                .models.find(
+                                                    (model) =>
+                                                        model.name ===
+                                                        selectedModel
+                                                ).summary.non_trainable
+                                        }
+                                    </span>
+                                </span>
+                            </nav>
+                        </div>
+                    )}
+                </div>
+            </section>
+        </div>
+    );
 };
 
-export default page;
+export default Page;
